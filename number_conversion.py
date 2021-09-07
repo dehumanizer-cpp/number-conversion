5ba09b71185bb7476f2b856955c6161bfbde3466
_hange <52858353+onur-py@users.noreply.github.com>
2021-09-08 02:17:57 +0300

    Update number_conversion.py

--- number_conversion.py
+++ number_conversion.py
@@ -1,38 +1,44 @@
-
 def binary_to_decimal():
 	k=8
 	ff=0
-
+	fy=["2","3","4","5","6","7","8","9"]
 	a=input()
-
-
+	for s in range(7):
+		if(len(a)!=8 or fy[s] in a):
+			exit()
+		else:
+			pass
 	for i in range(8):
 		
 		ff+=(int(a[i])*pow(2,k-1))
 		k-=1
 
-	print(ff)
+	print(f"\n>>>{ff}")
 
 def decimal_to_binary():
-	ff=[]
-
+	
 
 	yy=int(input())
+	if(yy>255 or yy<128):
+		exit()
+	else:
+	
 
-	for i in range(8):
-		
-		if(yy%2==0):
-			ff.append(0)
-			yy=int(yy/2)
-		else:
-			ff.append(1)
-			yy=int(yy/2)
-	ff1=""
-	for i in range(-7,1):
-		ff1+=str(ff[-i])
+		ff=[]
+		for i in range(8):
+			
+			if(yy%2==0):
+				ff.append(0)
+				yy=int(yy/2)
+			else:
+				ff.append(1)
+				yy=int(yy/2)
+		ff1=""
+		for i in range(-7,1):
+			ff1+=str(ff[-i])
 
 
-	print(ff1)
+		print(f"\n>>>{ff1}")
 
 
 select=input("1)Binary to Decimal\n2)Decimal to Binary\n\n>>>")
@@ -42,6 +48,6 @@ if(select=="1"):
 elif(select=="2"):
 	decimal_to_binary()
 else:
-	exit()
+	exit())
 
 
