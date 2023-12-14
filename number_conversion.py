4411e7c7d3dd8733d94d719641c34e41497ac77b
hangezoe <52858353+dehumanizer-cpp@users.noreply.github.com>
2023-12-15 00:23:18 +0300

    rewrited
    variables names and algorithm are edited

--- number_conversion.py
+++ number_conversion.py
@@ -1,71 +1,50 @@
-import os
-try:
-	def binary_to_decimal():
-		os.system("cls")
-		print("\t\tBINARY TO DECIMAL\n")
-		
-		kk=0
-		fy=["2","3","4","5","6","7","8","9"]
-		a0=int(input("t: "))
-		while a0>kk:
-			k=8
-			ff=0
-			a=input(">>>")
-			
-			for s in range(7):
-				if(len(a)!=8 or fy[s] in a):
-					os.system("pause")
-				else:
-					pass
-			for i in range(8):
-				ff+=(int(a[i])*pow(2,k-1))
-				k-=1
+selectedOption = "Null"
+def decimalToBinary(decimalValue,digit=8):
+    print("*--- DECIMAL TO BINARY")
 
-			print(f"\n>>>{ff}\n")
-			
-			kk+=1
-		os.system("pause")
-	
-	
-	
-	def decimal_to_binary():
-		os.system("cls")
-		print("\t\tDECIMAL TO BINARY\n")
-		
-		kk=0
-		a0=int(input("t: "))
-		while a0>kk:
-			ff1=""
-			yy=int(input(">>>"))
-			if(yy>255 or yy<128):
-				os.system("pause")
-			else:
-				ff=[]
-				for i in range(8):
-					
-					if(yy%2==0):
-						ff.append(0)
-						yy=int(yy/2)
-					else:
-						ff.append(1)
-						yy=int(yy/2)
-				
-				for i in range(-7,1):
-					ff1+=str(ff[-i])
+    listOfZeroAndOne=[]
+    for i in range(digit+1):
+        if (decimalValue % 2 ==0 ):
+            listOfZeroAndOne.append(0)
+        else:
+            listOfZeroAndOne.append(1)
+        decimalValue //= 2
+    return ''.join(str(e) for e in listOfZeroAndOne[::-1])#Reversing list and converting list to string
+
+def binaryToDecimal(binaryValue):
+    print("*--- BINARY TO DECIMAL")
+
+    binaryValue = str(binaryValue)
+    binaryToDecimalValue = 0
+    indexOfText = 0
+    for letterOnBinaryValue in binaryValue[::-1]:
+        if letterOnBinaryValue == "1":
+            binaryToDecimalValue += pow(2,indexOfText)
+            indexOfText += 1
+        else:
+            indexOfText += 1
+
+    return binaryToDecimalValue
+
+while selectedOption != "99":
+    selectedOption = input(
+"""
+1) Binary to decimal
+2) Decimal to binary
+>>>"""
+    )
+
+    if selectedOption == "1":
+        firstOptionInput = input("Binary value>>> ")
+        print(f"{firstOptionInput}'s decimal format is {binaryToDecimal(firstOptionInput)}")
+    elif selectedOption == "2":
+        secondOptionInput = int(input("Decimal value>>> "))
+        secondOptionInputDigit = int(input("-optional- Digit>>> "))
+        print(f"{secondOptionInput}'s binary format is {decimalToBinary(secondOptionInput,digit=secondOptionInputDigit)}")
+    elif selectedOption == "99":
+        print("Program will be closed.")
+        break
+    else:
+        print("You chose wrong option, try again.")
 
 
-				print(f"\n>>>{ff1}\n")
-				
-			kk+=1
-		os.system("pause")
-
-	select=input("1)Binary to Decimal\n2)Decimal to Binary\n\n>>>")
-
-	if(select=="1"):
-		binary_to_decimal()
-	elif(select=="2"):
-		decimal_to_binary()
-	else:
-		os.system("pause")
-except ValueError:
-	os.system("pause")
